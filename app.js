904af8601a52bcbfae050cd7584a3c40cd268c6b
赖维健 <01573241@yto.net.cn>
2019-08-16 16:56:00 +0800

    修改eslint配置

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ module.exports = app => {
   // 自定义内容
   app.projectName = 'eggManual'
 
+  /* hooks */
+
   // 配置文件即将加载
   app.configWillLoad(() => {})
 
-  app.beforeStart(async () => {
-    // 应用会等待这个函数执行完成才启动
-  })
+  app.beforeStart(async () => {})
 
   app.ready(async () => {})
 
-  app.beforeClose(async () => {
-    // console.log('==app beforeClose==')
-  })
+  app.beforeClose(async () => {})
 }
