43ff2691194e585a19a9e2985a6b9df34a32033b
赖维健 <01573241@yto.net.cn>
2019-08-16 13:57:21 +0800

    新增git提交

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ module.exports = app => {
   })
 
   app.beforeStart(async () => {
-      // 应用会等待这个函数执行完成才启动
-      console.log("==app beforeStart==");
-  });
+    // 应用会等待这个函数执行完成才启动
+    console.log('==app beforeStart==')
+  })
 
   app.ready(async () => {
-      console.log("==app ready==");
+    console.log('==app ready==')
   })
 
   app.beforeClose(async () => {
-      console.log("==app beforeClose==");
+    console.log('==app beforeClose==')
   })
-};
+}
